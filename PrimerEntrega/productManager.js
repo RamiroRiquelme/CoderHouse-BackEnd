604a6c944beebcb9821921fef46c6aef1918b751
Ramiro Valentin Riquelme <89102749+RamiroRiquelme@users.noreply.github.com>
2024-02-05 21:47:22 -0300

    Create productManager.js

---
+++ PrimerEntrega/productManager.js
@@ -0,0 +1,70 @@
+class ProductManager {
+    constructor() {
+      this.products = [];
+      this.productIdCounter = 1;
+    }
+  
+    addProduct(title, description, price, thumbnail, code, stock) {
+      // Valido que se completen los campos obligatorios
+      if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.error("Todos los campos son obligatorios");
+        return;
+      }
+  
+      // Valido que el código no se repita
+      if (this.products.some(product => product.code === code)) {
+        console.error("Ya existe un producto con ese código");
+        return;
+      }
+  
+      // Agrego el prodcuto con el id incrementable
+      const newProduct = {
+        id: this.productIdCounter++,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+      };
+  
+      this.products.push(newProduct);
+      console.log("Producto agregado:", newProduct);
+    }
+  
+    getProducts() {
+      return this.products;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find(product => product.id === id);
+  
+      if (product) {
+        return product;
+      } else {
+        console.error("Producto no encontrado");
+      }
+    }
+  }
+  
+  // Pruebas
+  const productManager = new ProductManager();
+  
+  // getProducts al inicio, debe devolver un arreglo vacío Este codigo devuelve el arreglo vacio
+  console.log("Productos al inicio:", productManager.getProducts());
+  
+  // addProduct con nuevos campos Añade nuevo propdcuto
+  productManager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+  
+  // getProducts después de agregar un producto debe contener el producto agregado
+  console.log("Productos después de agregar:", productManager.getProducts());
+  
+  // addProduct con los mismos campos, debe arrojar un error product manager con ejemplo repetido para que tire el error
+  productManager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+  
+  // getProductById con un ID que no existe, debe arrojar un error Buscamos coin un id que no existe 
+  productManager.getProductById(999);
+  
+  //getProductById con el ID del producto agregado, debe devolver el producto
+  const productById = productManager.getProductById(1);
+  console.log("Producto por ID:", productById);
